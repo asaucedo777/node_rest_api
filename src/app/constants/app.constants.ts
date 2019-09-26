8e2b228877f0167976fd6b900382db9e2a3b1366
asaucedovillanueva <alejandro.saucedovillanueva@soprasteria.com>
2019-09-26 16:36:18 +0200

    app folders

---
+++ src/app/constants/app.constants.ts
@@ -0,0 +1,6 @@
+export const ENVIRONMENT_ENV = 'env';
+export const ENVIRONMENT_PROTOCOL = 'protocol';
+export const ENVIRONMENT_URL = 'url';
+export const ENVIRONMENT_PORT = 'port';
+
+export const ROUTE_HOME = '/';
