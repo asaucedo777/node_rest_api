2348d88754b8972c51689a85a6ebb2a080128614
asaucedovillanueva <alejandro.saucedovillanueva@soprasteria.com>
2019-09-30 11:07:39 +0200

    Routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 
-import * as homeController from '../app/controllers/home.controller';
-import * as userController from '../app/controllers/user.controller';
 import { ENVIRONMENT, PORT, PROTOCOL, URL } from '../environments/environment';
 import { ENVIRONMENT_ENV, ENVIRONMENT_PORT, ENVIRONMENT_PROTOCOL,
-  ENVIRONMENT_URL, ROUTE_HOME, ROUTE_USERS, ROUTE_USERS_ID } from './constants/app.constants';
+  ENVIRONMENT_URL } from './constants/app.constants';
+import router from './router';
 
 const app = express();
 
@@ -16,13 +15,6 @@ app.set(ENVIRONMENT_PORT, PORT);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get(ROUTE_HOME, homeController.getHome);
-app.get(ROUTE_USERS, userController.getAll);
-app.get(ROUTE_USERS_ID, userController.getById);
-app.post(ROUTE_USERS, userController.create);
-app.put(ROUTE_USERS, userController.update);
-app.patch(ROUTE_USERS, userController.modify);
-app.delete(ROUTE_USERS_ID, userController.erase);
+app.use(router);
 
 export default app;
